fe8f59ef23f0bd21ba716cf7acba1e7427ebda46
Mauricio Fuentes <mauriciofb@gmail.com>
2024-05-14 18:43:12 -0400

    Ejercicio Club Deportivo

---
+++ src/index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+const fs = require('fs/promises')
+const { threadId } = require('worker_threads')
+
+app.listen(3000, () => console.log("APP LISTEN IN PORT 3000"))
+
+app.use(express.static("../static"))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html")
+})
+
+app.get("/deportes", (req, res) => {
+  fs.readFile("./data/deportes.json", "utf-8")
+    .then(data => {
+      const sports = JSON.parse(data)
+
+      res.setHeader("status", 200).json(sports)
+    })
+})
+
+app.get("/agregar", (req, res) => {
+  const { nombre, precio } = req.query
+
+  fs.readFile("./data/deportes.json", "utf-8")
+    .then(data => {
+      const sports = JSON.parse(data)
+
+      sports.deportes.push({nombre, precio})
+
+      fs.writeFile("./data/deportes.json", JSON.stringify(sports))
+      .then(() => {
+          res.json(sports)
+        })
+    })
+})
+
+app.get("/eliminar", (req, res) => {
+  const { nombre, precio } = req.query
+
+  fs.readFile("./data/deportes.json", "utf-8")
+    .then(data => {
+      const sportsJSON = JSON.parse(data)
+
+      const filteredSports = sportsJSON.deportes.filter( s => s.nombre !== nombre)
+
+      sportsJSON.deportes = filteredSports
+
+      fs.writeFile("./data/deportes.json", JSON.stringify(sportsJSON))
+      .then(
+        res.json(sportsJSON)
+        )
+    })
+})
+
+app.get("/editar", (req, res) => {
+  const { nombre, precio } = req.query
+
+  fs.readFile("./data/deportes.json", "utf-8")
+    .then(data => {
+      const sportsJSON = JSON.parse(data)
+
+      const modifiedSports = sportsJSON.deportes.map(s => {
+        if (s.nombre == nombre) {
+          s.precio = precio
+          return s
+        } else {
+          return s
+        }
+      })
+
+      sportsJSON.deportes = modifiedSports
+
+      fs.writeFile("./data/deportes.json", JSON.stringify(sportsJSON))
+        .then(() => {
+          res.json(sportsJSON)
+        })
+    })
+})
